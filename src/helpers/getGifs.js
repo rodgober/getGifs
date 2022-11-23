584542e25da29476b4544100f5f878c4b4b78aed
Rodrigo Bernardino <rodgober7@hotmail.com>
2022-11-23 16:59:15 -0600

    First version

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs =  async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=oO0PgDf06RCw9R8Op8XJZPsMpy0Ax1DR&q=${ category }&limit=5`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+
+    }))
+    return gifs;
+}
